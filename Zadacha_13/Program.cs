1fc0333442454ff733d394522fe6cdd04fa68b47
yana <wyemelt@gmail.com>
2022-12-13 21:18:31 +0300

    Zadacha_13 done

---
+++ Zadacha_13/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear ();
+Console.Write("Введите число: ");
+
+int number = int.Parse(Console.ReadLine());
+if (number > 100)
+{
+    string str = number + string.Empty;
+    Console.Write($"Третья цифра этого числа: {str[2]}");
+}
+else 
+{
+    Console.Write("Третей цифры нет");
+}
