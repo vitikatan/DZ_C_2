6f0e7ef337cf3018de6af06a06767f09e16586de
yana <wyemelt@gmail.com>
2022-12-13 21:45:10 +0300

    исправление ошибок

--- Zadacha_15/Program.cs
+++ Zadacha_15/Program.cs
@@ -3,7 +3,6 @@ Console.Write("Введите цифру: ");
 
 int number = int.Parse(Console.ReadLine());
 
-int[]week = {1, 2, 3, 4, 5, 6, 7, 8};
 if (number > 7)
 {
     Console.Write("Значение не соответствует дню недели");
