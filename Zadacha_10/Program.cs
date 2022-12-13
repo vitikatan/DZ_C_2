e656d9ed387796b9b6f04eb852a9401349d50f77
yana <wyemelt@gmail.com>
2022-12-13 20:59:19 +0300

    Zadacha_10 done

---
+++ Zadacha_10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Clear ();
+Console.Write("Введите трехзначное число: ");
+
+int number = int.Parse(Console.ReadLine());
+int number1 = number / 100;
+int number2 = (number - number1 * 100) / 10;
+
+Console.Write($"Вторая цифра этого числа: {number2}");
