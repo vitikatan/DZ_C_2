ebe90ea7e191d2241f0a1cf6f386b4e0fa9871ae
yana <wyemelt@gmail.com>
2022-12-13 21:36:03 +0300

    Zadacha_15 done

---
+++ Zadacha_15/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear ();
+Console.Write("Введите цифру: ");
+
+int number = int.Parse(Console.ReadLine());
+
+int[]week = {1, 2, 3, 4, 5, 6, 7, 8};
+if (number > 7)
+{
+    Console.Write("Значение не соответствует дню недели");
+}
+else
+{
+    if (number > 5)
+    {
+        Console.Write("Да");
+    }
+    else 
+    {
+        Console.Write("Нет");
+    }
+}
